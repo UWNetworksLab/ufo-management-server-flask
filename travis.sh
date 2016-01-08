a843498219a694e16143c318aee305c222b1a208
Ethan Holder <xwsxethan@gmail.com>
2016-01-08 11:43:33 -0800

    Travis script isn't running so trying to fix it.

--- travis.sh
+++ travis.sh
@@ -4,7 +4,6 @@
 # allows script to be run from different directories but always act on the
 # directory it is within.
 ROOT_DIR="$(cd "$(dirname $0)"; pwd)";
-UP_DIR="$(cd ..; pwd)";
 
 
 # A simple bash script to run commands to setup and install all dev
@@ -18,23 +17,7 @@ function runAndAssertCmd ()
     set -e && cd $ROOT_DIR && eval $1
 }
 
-function runInUfOAndAssertCmd ()
-{
-  echo "Running: $1"
-  # We use set -e to make sure this will fail if the command returns an error
-  # code.
-  if [ -d  "$UFO_MS_LOCAL_OTHER_LIB" ]; then
-    echo "In: $UFO_MS_LOCAL_OTHER_LIB"
-    echo
-    set -e && cd $UFO_MS_LOCAL_OTHER_LIB && eval $1
-  else
-    echo "In: $UFO_MS_LOCAL_DIR"
-    echo
-    set -e && cd $UFO_MS_LOCAL_DIR && eval $1
-  fi
-}
-
-function testOntravis ()
+function testOnTravis ()
 {
   runAndAssertCmd "pip install virtualenv"
   runAndAssertCmd "virtualenv venv"
@@ -44,4 +27,4 @@ function testOntravis ()
   runAndAssertCmd "npm install"
 }
 
-testOntravis
+testOnTravis
