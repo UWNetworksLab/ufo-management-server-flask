207121950248504b6768e5bd7c6201f7c7525c34
Henry Chang <henryc@google.com>
2016-02-04 10:34:48 -0800

    update requirements.txt and clock.py docstring

--- clock.py
+++ clock.py
@@ -1,3 +1,8 @@
+"""This module schedules all the periodic batch processes on heroku.
+
+https://devcenter.heroku.com/articles/clock-processes-python
+"""
+
 import proxy_server
 
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -7,7 +12,7 @@ scheduler = BlockingScheduler()
 
 @scheduler.scheduled_job('interval', minutes=15)
 def distribute_user_keys_to_proxy_servers():
-  """Schedule the user key distribution to proxy servers."""
+  """Schedule the user key distribut to proxy servers."""
   print 'Start scheduling key distribution to proxy servers.'
   proxy_server.distribute_keys()
   print 'Finished scheduling key distribution to proxy servers.'
