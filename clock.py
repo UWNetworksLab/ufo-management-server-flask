8bce60d2a594f2742db2aaef573555337a774ec2
Henry Chang <henryc@google.com>
2016-02-04 10:30:11 -0800

    add docstring

--- clock.py
+++ clock.py
@@ -7,6 +7,7 @@ scheduler = BlockingScheduler()
 
 @scheduler.scheduled_job('interval', minutes=15)
 def distribute_user_keys_to_proxy_servers():
+  """Schedule the user key distribution to proxy servers."""
   print 'Start scheduling key distribution to proxy servers.'
   proxy_server.distribute_keys()
   print 'Finished scheduling key distribution to proxy servers.'
