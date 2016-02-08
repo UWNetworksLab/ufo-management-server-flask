31e1da903dee0f12e7931b994615fffaa213b1e8
Henry Chang <henryc@google.com>
2016-02-08 10:54:14 -0800

    make the schedule shorter for testing

--- clock.py
+++ clock.py
@@ -9,7 +9,7 @@ import ufo
 
 SCHEDULER = BlockingScheduler()
 
-@SCHEDULER.scheduled_job('interval', minutes=15)
+@SCHEDULER.scheduled_job('interval', seconds=5)
 def distribute_user_keys_to_proxy_servers():
   """Schedule the user key distribution to proxy servers."""
   ufo.proxy_server.distribute_keys()
