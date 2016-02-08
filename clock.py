818286e26e4efe642e97380176dc9fe7f2ed73f2
Henry Chang <henryc@google.com>
2016-02-08 10:39:45 -0800

    remove the print statements as they don't seem to do anything either

--- clock.py
+++ clock.py
@@ -12,9 +12,7 @@ SCHEDULER = BlockingScheduler()
 @SCHEDULER.scheduled_job('interval', minutes=15)
 def distribute_user_keys_to_proxy_servers():
   """Schedule the user key distribution to proxy servers."""
-  # TODO: Get rid of the print by having a logger that redirects to stdout.
-  print 'Start scheduling key distribution to proxy servers.'
   ufo.proxy_server.distribute_keys()
-  print 'Finished scheduling key distribution to proxy servers.'
+
 
 SCHEDULER.start()
