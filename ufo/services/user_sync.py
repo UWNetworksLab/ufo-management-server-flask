4cea45c8bb0324180c3041d08b5d7dfbf047d293
Ethan Holder <xwsxethan@gmail.com>
2016-03-01 13:52:14 -0800

    Giving user sync app context since that is apparently necessary.

--- ufo/services/user_sync.py
+++ ufo/services/user_sync.py
@@ -21,60 +21,61 @@ class UserSync(object):
     and will thus be removed from our DB.
     """
     db_users = models.User.query.all()
-    config = ufo.get_user_config()
-    credentials = oauth.getSavedCredentials()
-    # TODO this should handle the case where we do not have oauth
-    if not credentials:
-      ufo.app.logger.info('OAuth credentials not set up. Can\'t sync users.')
-
     directory_users = {}
-    try:
-      directory_service = google_directory_service.GoogleDirectoryService(
-          credentials)
-      directory_users = directory_service.GetUsersAsDictionary()
+    config = ufo.get_user_config()
+    with ufo.app.app_context():
+      credentials = oauth.getSavedCredentials()
+      # TODO this should handle the case where we do not have oauth
+      if not credentials:
+        ufo.app.logger.info('OAuth credentials not set up. Can\'t sync users.')
 
-    except errors.HttpError as error:
-      ufo.app.logger.info('Error encountered while requesting users from '
-                          'directory service: ' + str(error))
-      return
+      try:
+        directory_service = google_directory_service.GoogleDirectoryService(
+            credentials)
+        directory_users = directory_service.GetUsersAsDictionary()
 
-    for db_user in db_users:
-      # Don't worry about users from another domain since they won't show up.
-      if db_user.domain != config.domain:
-        ufo.app.logger.info('User ' + db_user.email + ' did not match the  '
-                            'current domain. Ignoring in directory service.')
-        continue
+      except errors.HttpError as error:
+        ufo.app.logger.info('Error encountered while requesting users from '
+                            'directory service: ' + str(error))
+        return
 
-      # Lookup user in dictionary based on email field.
-      directory_user = directory_users.get(db_user.email, default=None)
-      ufo.app.logger.info('Found user: ' + str(directory_user))
+      for db_user in db_users:
+        # Don't worry about users from another domain since they won't show up.
+        if db_user.domain != config.domain:
+          ufo.app.logger.info('User ' + db_user.email + ' did not match the  '
+                              'current domain. Ignoring in directory service.')
+          continue
 
-      # Assume deleted if not found, so delete from our db.
-      if directory_user is None:
-        if config.user_delete_action is models.CRON_JOB_ACTIONS['delete']:
-          ufo.app.logger.info('User ' + db_user.email + ' was not found in '
-                              'directory service. Deleting from database.')
-          db_user.delete()
-        elif config.user_delete_action is models.CRON_JOB_ACTIONS['revoke']:
-          ufo.app.logger.info('User ' + db_user.email + ' was not found in '
-                              'directory service. Revoking access.')
-          db_user.is_key_revoked = True
-          db_user.did_cron_revoke = True
-          db_user.save()
-        continue
+        # Lookup user in dictionary based on email field.
+        directory_user = directory_users.get(db_user.email, default=None)
+        ufo.app.logger.info('Found user: ' + str(directory_user))
 
-      if directory_user['suspended']:
-        if config.user_revoke_action is models.CRON_JOB_ACTIONS['delete']:
-          ufo.app.logger.info('User ' + db_user.email + ' was suspended in '
-                              'directory service. Deleting from database.')
-          db_user.delete()
-        elif config.user_revoke_action is models.CRON_JOB_ACTIONS['revoke']:
-          ufo.app.logger.info('User ' + db_user.email + ' was suspended in '
-                              'directory service. Revoking access.')
-          db_user.is_key_revoked = True
-          db_user.did_cron_revoke = True
-          db_user.save()
-        continue
+        # Assume deleted if not found, so delete from our db.
+        if directory_user is None:
+          if config.user_delete_action is models.CRON_JOB_ACTIONS['delete']:
+            ufo.app.logger.info('User ' + db_user.email + ' was not found in '
+                                'directory service. Deleting from database.')
+            db_user.delete()
+          elif config.user_delete_action is models.CRON_JOB_ACTIONS['revoke']:
+            ufo.app.logger.info('User ' + db_user.email + ' was not found in '
+                                'directory service. Revoking access.')
+            db_user.is_key_revoked = True
+            db_user.did_cron_revoke = True
+            db_user.save()
+          continue
+
+        if directory_user['suspended']:
+          if config.user_revoke_action is models.CRON_JOB_ACTIONS['delete']:
+            ufo.app.logger.info('User ' + db_user.email + ' was suspended in '
+                                'directory service. Deleting from database.')
+            db_user.delete()
+          elif config.user_revoke_action is models.CRON_JOB_ACTIONS['revoke']:
+            ufo.app.logger.info('User ' + db_user.email + ' was suspended in '
+                                'directory service. Revoking access.')
+            db_user.is_key_revoked = True
+            db_user.did_cron_revoke = True
+            db_user.save()
+          continue
 
   def enqueue_user_sync(self):
     """Check users currently in the DB are still valid in directory service.
