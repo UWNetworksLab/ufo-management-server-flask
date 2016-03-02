db09350ba65152c3366f03199c8bb39dc69bd6a7
Ethan Holder <xwsxethan@gmail.com>
2016-03-02 09:46:32 -0800

    Adding IP v6 regex into the mix for parsing many different IP addresses correctly on the client side.

--- ufo/__init__.py
+++ ufo/__init__.py
@@ -40,10 +40,29 @@ PRIVATE_KEY_ERROR = ('Please supply a valid private key in a format such as '
                      '-----END RSA PRIVATE KEY-----"')
 app.jinja_env.globals['PRIVATE_KEY_PATTERN'] = PRIVATE_KEY_PATTERN
 app.jinja_env.globals['PRIVATE_KEY_ERROR'] = PRIVATE_KEY_ERROR
-# TODO(eholder): Find a regex for ipv6 then or these two together.
-IP_ADDRESS_KEY_PATTERN = (r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}'
-                          r'(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
-IP_ADDRESS_KEY_ERROR = ('Please supply a valid ip address in v4 format.')
+# For information on where this huge regex came from, see here:
+# http://stackoverflow.com/a/17871737/2216222
+IP_V6_REGEX = (r'(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|'
+               r'([0-9a-fA-F]{1,4}:){1,7}:|'
+               r'([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|'
+               r'([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|'
+               r'([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|'
+               r'([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|'
+               r'([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|'
+               r'[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|'
+               r':((:[0-9a-fA-F]{1,4}){1,7}|:)|'
+               r'fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|'
+               r'::(ffff(:0{1,4}){0,1}:){0,1}'
+               r'((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}'
+               r'(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|'
+               r'([0-9a-fA-F]{1,4}:){1,4}:'
+               r'((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}'
+               r'(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])'
+               r')')
+IP_V4_REGEX = (r'((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|'
+               r'(2[0-4]|1{0,1}[0-9]){0,1}[0-9])')
+IP_ADDRESS_KEY_PATTERN = IP_V6_REGEX + r'|' + IP_V4_REGEX
+IP_ADDRESS_KEY_ERROR = ('Please supply a valid ip address in v4 or v6 format.')
 app.jinja_env.globals['IP_ADDRESS_KEY_PATTERN'] = IP_ADDRESS_KEY_PATTERN
 app.jinja_env.globals['IP_ADDRESS_KEY_ERROR'] = IP_ADDRESS_KEY_ERROR
 
