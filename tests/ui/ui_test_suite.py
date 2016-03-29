23f11e1140d5cd4614dfdf7e666bf3af6ef6d95d
Henry Chang <henryc@google.com>
2016-03-29 16:31:39 -0700

    Fix import.

--- tests/ui/ui_test_suite.py
+++ tests/ui/ui_test_suite.py
@@ -3,7 +3,7 @@ import argparse
 import unittest
 
 from admin_flow_test import AdminFlowTest
-from landing_page_test import ErrorNotificationPageTest
+from landing_page_test import ErrorNotificationTest
 from landing_page_test import LandingPageTest
 from login_page_test import LoginPageTest
 from search_page_test import SearchPageTest
