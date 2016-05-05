dc644dea11f2687154b142c95fe0b225cc9c1bb5
Ethan Holder <xwsxethan@gmail.com>
2016-05-05 08:32:59 -0700

    Fix copy invite code. We have to use a different event listener format for it to trigger properly in Chrome. Kind of a pain, but it should work now. Also cleaned up how it functions by having one static hidden node in the dom to put the content onto then copy from (rather than dynamically generating and destroying a node each time). Finally, added icons to the copy and create new code buttons.

--- ufo/static/globalHelperFunctions.js
+++ ufo/static/globalHelperFunctions.js
@@ -13,3 +13,19 @@ var submitUsersManually = function() {
   input.value = JSON.stringify(userArray);
   formElem.submit();
 };
+
+
+(function() {
+  'use strict';
+  document.body.addEventListener('copy', copyInviteCodeToClipboard, true);
+
+  function copyInviteCodeToClipboard(e) {
+    if (e.target !== document.body) {
+      return;
+    }
+    e.preventDefault();
+
+    var input = document.getElementById('hiddenCopyInput');
+    e.clipboardData.setData('text/plain', input.value);
+  }
+})();
