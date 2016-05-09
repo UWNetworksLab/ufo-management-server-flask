f5d4d25485bad414a4a7b3c35074df952caf787c
Ethan Holder <xwsxethan@gmail.com>
2016-05-09 11:28:09 -0700

    Adding comment to JS for why we're using the event handler there and not in Polymer.

--- ufo/static/globalHelperFunctions.js
+++ ufo/static/globalHelperFunctions.js
@@ -14,7 +14,13 @@ var submitUsersManually = function() {
   formElem.submit();
 };
 
-
+/**
+ * I had to do this outside of a custom element because of the way that the
+ * browser handles permissions for using the document.execCommand and
+ * clipboardData API's. In essence, it doesn't like how Polymer registers and
+ * fire event handlers and won't let you touch clipboardData from there, thus
+ * the Javascript is outside of Polymer.
+ */
 (function() {
   'use strict';
   document.body.addEventListener('copy', copyInviteCodeToClipboard, true);
