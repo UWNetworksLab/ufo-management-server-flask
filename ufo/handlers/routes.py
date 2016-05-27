7bf133573c96aa51d26bd1acd88d9a506babcbbb
Ethan Holder <xwsxethan@gmail.com>
2016-05-27 13:28:25 -0700

    This is just initial work to save.

--- ufo/handlers/routes.py
+++ ufo/handlers/routes.py
@@ -1,6 +1,7 @@
 import json
 
 import flask
+from flask import request
 
 import ufo
 from ufo.handlers import auth
@@ -13,7 +14,10 @@ from ufo.handlers import user
 @ufo.setup_required
 @auth.login_required
 def landing():
-
+  print "Headers"
+  print "Accept: " + request.headers.get('Accept')
+  print "Accept-Encoding: " + request.headers.get('Accept-Encoding')
+  print "Accept-Language: " + request.headers.get('Accept-Language')
   return flask.render_template('landing.html')
 
 
